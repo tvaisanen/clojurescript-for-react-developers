3854195e0f8d1f017dc256bd8275dd9c7a30fb59
tvaisanen <vaisanen.toni@gmail.com>
2023-02-26 19:13:10 +0200

    set up react

--- introduction/app/src/main/app/core.cljs
+++ introduction/app/src/main/app/core.cljs
@@ -1,17 +1,35 @@
 ;; core is used often in clojure similarly as index.js in Javascript
-(ns app.core)
+(ns app.core
+  (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            ["react" :as react]
+            ["react-dom/client" :as rdom]))
 
-(defn start
-  "Shadow CLJS will run this code everytime on after load."
-  []
-  (prn "app start"))
+;; `defnc` macro creates a react component
+(defnc App []
+  (d/div "This is a React component."))
 
-(defn stop
-  "Shadow CLJS will run this code everytime before load."
-  []
-  (prn "app stop"))
+(defn app-container []
+  (js/document.getElementById "app"))
 
-(defn init
-  "This function is used in index.html to load the application."
+(defonce root (atom nil))
+
+(defn react-root []
+  (when-not @root
+    ;; createRoot should be done only once so
+    ;; let's store the reference to `root`
+    ;; if not already exist
+    (reset! root (rdom/createRoot (app-container))))
+  @root)
+
+;; add ^:dev/after-load to re-mount the app on file change
+;; OR update the shadow-cljs.edn config file to run
+;; app.core.init on after-load
+(defn ^:dev/after-load init
+  "This function is used in the `index.html`
+  to load the application."
   []
-  (js/console.log "Browser loaded the code"))
+  (.render (react-root)
+           ;; `$` is a macro to make a React
+           ;; element out of the given component
+           ($ react/StrictMode ($ App))))
